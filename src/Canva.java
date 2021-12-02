4a9aadda1ddae12bf56abbe98f6c6918a7f4c7f6
Paulina Szywała <paulinaszywala01@gmail.com>
2021-12-02 12:42:17 +0100

    Dodajemy klasę Canva oraz KeyListener do klasy Window

---
+++ src/Canva.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Canva extends JPanel {
+
+    public Canva(){
+
+        this.setBackground(Color.PINK);
+    }
+}
--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements KeyListener {
+
+    Canva canva;
 
     public Window(){
 
@@ -10,11 +14,39 @@ public class Window extends JFrame {
         this.setSize(500,500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel canva = new JPanel();
+        /*JPanel canva = new JPanel();
         canva.setBackground(Color.BLUE);
+        this.add(canva);*/
+
+        canva = new Canva();
         this.add(canva);
+
+
+
 
     }
 
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+        switch (e.getKeyChar())
+        {
+            case 'k':
+            {
+            
+            }
+        }
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
