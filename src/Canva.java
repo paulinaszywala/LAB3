7ec04489f7f43ca8e98e81b758b166eef9f71e76
Paulina Szywała <paulinaszywala01@gmail.com>
2021-12-02 14:29:20 +0100

    Koła i kwadraty dodane do klasy window, mniej więcej zaczyna działać

--- src/Canva.java
+++ src/Canva.java
@@ -1,10 +1,80 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Canva extends JPanel {
+public class Canva extends JPanel implements MouseListener, KeyListener {
+
+    JLabel label;
+    ImageIcon oval;
+    ImageIcon square;
 
     public Canva(){
 
         this.setBackground(Color.PINK);
+        this.setPreferredSize(new Dimension(500,500));
+
+
+
+
+
+    }
+
+
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+        switch (e.getKeyChar()){
+            case 'o': label.setIcon(oval);
+            break;
+            case 'k': label.setIcon(square);
+        }
+
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+
+
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+        this.add(label);
+        label.setLocation(e.getX(),e.getY());
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -2,10 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Window extends JFrame implements KeyListener {
+public class Window extends JFrame implements KeyListener, MouseListener {
 
     Canva canva;
+    JLabel label;
+    ImageIcon oval;
+    ImageIcon square;
+
 
     public Window(){
 
@@ -13,31 +19,45 @@ public class Window extends JFrame implements KeyListener {
         this.setTitle("Canva");
         this.setSize(500,500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        this.pack();
+        this.addMouseListener(this);
+        this.addKeyListener(this);
+
+        oval = new ImageIcon("oval.png");
+        square = new ImageIcon("square.png");
+
+        label = new JLabel();
+        label.addKeyListener(this);
+
+        label.setIcon(oval);
+
+        this.add(label);
+        label.setVisible(false);
+
+        //this.setLocationRelativeTo(null);
+
 
         /*JPanel canva = new JPanel();
         canva.setBackground(Color.BLUE);
         this.add(canva);*/
 
-        canva = new Canva();
-        this.add(canva);
-
-
-
+       // canva = new Canva();
+        //this.add(canva);
 
     }
+
 
 
     @Override
     public void keyTyped(KeyEvent e) {
 
-        switch (e.getKeyChar())
-        {
-            case 'k':
-            {
-            
-            }
+        switch (e.getKeyChar()){
+            case 'o': label.setIcon(oval);
+                break;
+            case 's': label.setIcon(square);
         }
-
     }
 
     @Override
@@ -47,6 +67,34 @@ public class Window extends JFrame implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        label.setVisible(true);
+        label.setLocation(e.getX(),e.getY());
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
